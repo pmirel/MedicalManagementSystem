df4c11e0d590e759f61622bb5d8e2739b43922e6
M <M@DESKTOP-J0D963F>
2020-07-03 12:21:30 +0300

    Validators for patient and prescription

--- MedicalManagementSystem/ModelValidators/PatientValidator.cs
+++ MedicalManagementSystem/ModelValidators/PatientValidator.cs
@@ -1,11 +1,33 @@
-﻿using System;
+using FluentValidation;
+using MedicalManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MedicalManagementSystem.ModelValidators
 {
-    public class PatientValidator
+    public class PatientValidator : AbstractValidator<Patient>
     {
+        public PatientValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.FirstName)
+                .Length(2, 10)
+                .WithMessage("Text must have minimum 2 characters and maximum 10.");
+            RuleFor(x => x.FirstName)
+                .NotEmpty()
+                .WithMessage("Text cannot be empty.");
+            RuleFor(x => x.LastName)
+                .Length(2, 10)
+                .WithMessage("Text must have minimum 2 characters and maximum 10.");
+            RuleFor(x => x.LastName)
+                .NotEmpty()
+                .WithMessage("Text cannot be empty.");
+            RuleFor(x => x.CNP)
+                .Length(13)
+                .WithMessage("CNP must have 13 numbers");
+        }
     }
+    
 }
--- MedicalManagementSystem/ModelValidators/PrescriptionValidator.cs
+++ MedicalManagementSystem/ModelValidators/PrescriptionValidator.cs
@@ -1,11 +1,30 @@
-﻿using System;
+using FluentValidation;
+using MedicalManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MedicalManagementSystem.ModelValidators
 {
-    public class PrescriptionValidator
+    public class PrescriptionValidator : AbstractValidator<Prescription>
     {
+        public PrescriptionValidator()
+        {
+            RuleFor(x => x.Id).NotNull();
+            RuleFor(x => x.Diagnosis)
+                .Length(2, 20)
+                .WithMessage("Text must have minimum 2 characters and maximum 20.");
+            RuleFor(x => x.Diagnosis)
+                .NotEmpty()
+                .WithMessage("Diagnosis cannot be empty.");
+            RuleFor(x => x.DateAdded)
+                .GreaterThanOrEqualTo(DateTime.Now)
+                .WithMessage("Date must be equal or higer tha today");
+            RuleFor(x => x.Price)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Price must be equal or higher than 0");
+        }
     }
 }
