fb8e64ab8b90ae34bb436a73b5dd4d5a62dcfaae
M <M@DESKTOP-J0D963F>
2020-07-03 12:10:20 +0300

    Added angular material

--- MedicalManagementSystem/ClientApp/src/app/app.module.ts
+++ MedicalManagementSystem/ClientApp/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { AngularMaterialModule } from './shared/angular-material.module';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,8 +29,11 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    AngularMaterialModule,
+    BrowserAnimationsModule
   ],
+  exports: [AngularMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ MedicalManagementSystem/ClientApp/src/app/shared/angular-material.module.ts
@@ -0,0 +1,90 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+
+    MatSlideToggleModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatListModule,
+    MatTableModule,
+    MatGridListModule,
+
+    MatFormFieldModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatChipsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatCardModule,
+
+    MatDatepickerModule,
+    MatNativeDateModule,
+
+} from '@angular/material';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MatSlideToggleModule,
+        MatSliderModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatBadgeModule,
+        MatListModule,
+        MatTableModule,
+        MatGridListModule,
+
+        MatFormFieldModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatChipsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTooltipModule,
+
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatProgressBarModule,
+        MatCardModule
+    ],
+    exports: [
+        MatSlideToggleModule,
+        MatSliderModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatBadgeModule,
+        MatListModule,
+        MatTableModule,
+        MatGridListModule,
+
+        MatFormFieldModule,
+        MatSelectModule,
+        MatRadioModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatChipsModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTooltipModule,
+
+        MatPaginatorModule,
+        MatProgressSpinnerModule,
+        MatProgressBarModule,
+        MatCardModule
+    ],
+})
+
+export class AngularMaterialModule { }
